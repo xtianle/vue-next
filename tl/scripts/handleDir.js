ea019db9749de54da6cc7d6b6bd17b98518c00ea
邢天乐 <1085075895@qq.com>
2021-09-12 16:00:00 +0800

    analysis(\me==>\tl): me rename tl

---
+++ tl/scripts/handleDir.js
@@ -0,0 +1,40 @@
+var fs = require('fs')
+var path = require('path')
+
+const url = '../../'
+
+const blackList = ['node_modules', '.idea', '.vscode', '.git', '.github', 'tl']
+
+function handleFullpath(fullpath, spaceNum) {
+  const reg1 = /\\/g
+  const reg2 = new RegExp(url + '/', 'g')
+  const fileOrDir = fullpath
+    .replace(reg1, '/')
+    .replace(reg2, '')
+    .split('/')
+    .pop()
+  const prefix = new Array(spaceNum).fill('│     ').join('') + '├── '
+  return prefix + fileOrDir + '\n'
+}
+
+function travel(dir, spaceNum, resultStr = '') {
+  var arr = fs.readdirSync(dir)
+
+  for (let i = 0, len = arr.length; i < len; i++) {
+    const item = arr[i]
+    if (blackList.indexOf(item) !== -1) {
+      continue
+    }
+
+    var fullpath = path.join(dir, item)
+    resultStr += handleFullpath(fullpath, spaceNum)
+    if (fs.statSync(fullpath).isDirectory()) {
+      resultStr += travel(fullpath, spaceNum + 1)
+    }
+  }
+
+  return resultStr
+}
+
+fs.writeFileSync(path.join(__dirname, '../result.md'), travel(url, 0))
+console.log('处理目录成功')
